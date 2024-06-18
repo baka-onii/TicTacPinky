f5b39ba127131c0e104bc0795d7d2bcab140c32b
baka-onii <104510536+baka-onii@users.noreply.github.com>
2024-06-18 22:17:32 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,10 +5,9 @@ let nextMoveInLargeCell = -1; // -1 means the player can choose any cell in the 
 
 // Winning combinations for mini boards
 const miniBoardWins = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-    [0, 4, 8], [2, 4, 6] // Diagonals
-];
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6] 
 
 function checkMiniBoardWinner(cells) {
     for (let combo of miniBoardWins) {
